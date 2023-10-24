688006477eaa5158ced6fe39617a741c7ae3f16d
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-24 19:11:20 +0400

    timer is working

--- utils/TimerBis.ts
+++ utils/TimerBis.ts
@@ -2,9 +2,8 @@ import { Server } from "socket.io";
 import { formatTime } from "./TimerBisUtils";
 
 export default class TimerBis {
-  private currentTime: number; // Le temps actuel restant sur le timer (en millisecondes)
-  // isRunning: boolean; // Indique si le timer est en cours d'exécution ou non
-  private timerInterval: NodeJS.Timeout; // Une référence à l'intervalle de temps (NodeJS.Timeout) créé pour mettre à jour le timer
+  private currentTime: number;
+  private timerInterval: NodeJS.Timeout;
   couleur: string;
   private io: Server;
   private roomId: string;
@@ -16,7 +15,6 @@ export default class TimerBis {
     roomId: string
   ) {
     this.currentTime = initialTime;
-    // this.isRunning = false;
     this.timerInterval = setInterval(() => {
       return;
     }, 1);
@@ -25,57 +23,19 @@ export default class TimerBis {
     this.io = io;
   }
 
-  /** Démarre le timer */
   public start(): void {
-    // Vérifie si le timer n'est pas déjà en cours d'exécution
-    console.log(`+++ Dans la méthode start ${this.couleur} +++`);
-    // if (!this.isRunning) {
-    //   this.isRunning = true; // Marque le timer comme en cours d'exécution
     // Crée un intervalle qui décrémentera le temps restant toutes les secondes
     this.timerInterval = setInterval(() => {
       this.currentTime -= 1000;
-      // console.log(
-      //   this.couleur +
-      //     " - " +
-      //     formatTime(this.currentTime) +
-      //     " - " +
-      //     this.isRunning +
-      //     " - " +
-      //     "this.timerInterval :" +
-      //     this.timerInterval
-      // );
+      // Emit updated time
       this.io.to(this.roomId).emit("timer", {
         currentTime: formatTime(this.currentTime),
         playerPiece: this.couleur,
       });
-      // Vérifie si le temps est écoulé
-      //   if (this.currentTime <= 0) {
-      //     this.stop();
-      //     // ! Mettre la fonction à executer quand timer atteint sa fin
-      //   }
     }, 1000);
-    console.log("this.timerInterval====>", this.timerInterval);
-    // }
   }
 
-  /** Arrête le timer en cours. */
   public stop(): void {
-    console.log(`+++ Dans la méthode stop ${this.couleur} +++`);
-    // Vérifie si le timer est en cours d'exécution
-    // console.log(this.couleur + " - " + "this.isRunning :" + this.isRunning);
-    // console.log(
-    //   this.couleur + " - " + "this.timerInterval :" + this.timerInterval
-    // );
-    // console.log("this.running", this.isRunning);
-    // if (this.isRunning) {
-    console.log("in if 01");
-    //   this.isRunning = false; // Marque le timer comme arrêté
-    // Arrête l'intervalle de mise à jour du temps, s'il existe
-    // if (this.timerInterval) {
-    console.log("in if 02");
     clearInterval(this.timerInterval);
-    // this.timerInterval = null;
-    // }
-    // }
   }
 }
--- webSocket.ts
+++ webSocket.ts
@@ -8,7 +8,6 @@ import {
   ChatMessageType,
 } from "./utils/room.utils";
 import { communication, onMove } from "./utils/webSocket.utils";
-import Timer from "./utils/Timer";
 import TimerBis from "./utils/TimerBis";
 
 export function webSocketConnection(socket: Socket, io: Server) {
@@ -17,56 +16,36 @@ export function webSocketConnection(socket: Socket, io: Server) {
   const player = updateRoomsWithSocketId(room, socket.id);
   const playerPiece = player == 1 ? PieceEnum.yellow : PieceEnum.red; // Pas une erreur ici mais intentionel (inversion=> p1:yellow ; p2:red)
 
-  const timer = 300000;
-  // const timerYellow = new Timer(timer, PieceEnum.yellow, io, roomId, () => {
-  //   console.log("Le timer jaune est terminé.");
-  // });
-  // const timerRed = new Timer(timer, PieceEnum.red, io, roomId, () => {
-  //   console.log("Le timer rouge est terminé.");
-  // });
-  // const timerYellow = new TimerBis(timer, PieceEnum.yellow, io, roomId);
-  let timerYellow: TimerBis;
-  console.log("new timer created"); // !!!!!!!PROBLÈME DE CONCURENCE!!!!!!!!!!!
-  let timerRed: TimerBis;
-  // const timerRed = new TimerBis(timer, PieceEnum.red, io, roomId);
+  const timerTime = 300000;
 
   console.log("new player (", playerPiece, ") connected in room", roomId);
 
   socket.emit("player color", playerPiece);
 
   socket.join(roomId);
-  // if (player == 1) {
-  // }
+
   if (player == 2) {
     io.to(roomId)
       .to(room.playerOneSocketId as string)
       .emit("opponent ready");
     // Démarrage des timer
-    //io.to(roomId).emit("timer", timerRed.getCurrentTime())
-    timerYellow = new TimerBis(timer, PieceEnum.yellow, io, roomId);
-    timerRed = new TimerBis(timer, PieceEnum.red, io, roomId);
-    timerRed.start();
+    room.redTimer = new TimerBis(timerTime, PieceEnum.red, io, roomId);
+    room.yellowTimer = new TimerBis(timerTime, PieceEnum.yellow, io, roomId);
+    room.redTimer.start();
   }
 
   socket.on("move", (req: PlayerMoveType) => {
     onMove(io, socket, req, room, roomId, playerPiece);
-    console.log(" ====== Joeur playerPiece: " + playerPiece + " a joué ======");
-    // Arreter d'envoie le timer
-    debugger;
+
+    // Envoie en continu du temps restant
     if (playerPiece === PieceEnum.red) {
-      console.log(
-        " --- " + playerPiece + " stop timer rouge - start timer jaune --- "
-      );
-      timerRed.stop();
-      timerYellow.start();
+      room.redTimer?.stop();
+      room.yellowTimer?.start();
     } else {
-      console.log(
-        " --- " + playerPiece + " stop timer jaune - start time rouge --- "
-      );
+      room.yellowTimer?.stop();
+      room.redTimer?.start();
     }
   });
-
-  // Envoie en continu du temps restant
 
   socket.on("message", (req: ChatMessageType) =>
     communication(io, roomId, playerPiece, req)
